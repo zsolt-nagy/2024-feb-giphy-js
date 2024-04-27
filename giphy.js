effe6ff2c65764034f8fbf0e264b0a5e4f9d6a28
Zsolt Nagy <info@zsoltnagy.eu>
2024-04-27 02:33:15 +0200

    styling

--- giphy.js
+++ giphy.js
@@ -1,1 +1,31 @@
+const API_KEY = "hPLiLRR4wSIPRRFML70Qd8kOK5itmBhY";
+const API_PREFIX = "https://api.giphy.com/v1/gifs/search?api_key=";
+const API_SETTINGS = "offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
+function formSubmitted(event) {
+    event.preventDefault();
+    let inputFieldContent = document.querySelector("[name=memeInput]").value;
+    getMemes(inputFieldContent);
+}
+
+function renderGifs(response) {
+    let result = "";
+
+    for (let meme of response.data) {
+        result += `
+            <img src="${meme.images.original.url}" alt="${meme.alt_text}" class="meme-img" />
+        `;
+    }
+
+    document.querySelector(".js-memes-container").innerHTML = result;
+}
+
+function getMemes(searchExpression) {
+    fetch(
+        `${API_PREFIX}${API_KEY}&q=${searchExpression}&limit=25&${API_SETTINGS}`
+    )
+        .then((data) => data.json())
+        .then(renderGifs);
+}
+
+document.querySelector("#memeForm").addEventListener("submit", formSubmitted);
